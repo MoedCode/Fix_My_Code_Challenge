bc8ebe9bea890b7a1fb09c43ccc037d6a854a46b
MoedCode <mohamed71291@gmail.com>
2024-01-30 14:40:33 +0200

    1. Export to CSV (automated command)

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+""" Square module. """
+
 
 class Square():
     """A class representing a square with width and height attributes."""
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
         """Initialize the Square with specified width and height."""
-        self.width = kwargs.get('width', 0)
-        self.height = kwargs.get('height', 0)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """Calculate the area of the square."""
+        return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """perimeter of the square."""
-        return self.width * 4
+    def permiter_of_my_square(self):
+        """Calculate the perimeter of the square."""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """String representation of the square's dimensions."""
@@ -23,7 +26,8 @@ class Square():
 
 
 if __name__ == "__main__":
+    """ Create Square instance """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
