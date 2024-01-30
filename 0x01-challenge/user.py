5420add1eb5a642c317683e0f6afa8b2409e4ebb
MoedCode <mohamed71291@gmail.com>
2024-01-30 15:01:48 +0200

    1. Export to CSV (automated command)

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -3,28 +3,27 @@
 User class
 """
 
+
 class User():
-    """ Documentation """
+    """ user class """
 
     __email = None
 
     def __init__(self):
-        """ Documentation """
+        """ init method """
         self.__email = None
 
     @property
     def email(self):
-        """ Documentation """
+        """ gets an email """
         return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
+        """ sets an email """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
-
-
 
 
 if __name__ == "__main__":
